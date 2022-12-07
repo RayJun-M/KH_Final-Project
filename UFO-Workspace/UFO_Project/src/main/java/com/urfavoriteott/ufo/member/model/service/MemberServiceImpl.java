d7cc94a2f63ec5ce76df26e4251107aeac60014c
hyne993 <110704609+hyne993@users.noreply.github.com>
2022-12-07 11:16:49 +0900

    2022-12-07 장희연 - 기능

--- UFO-Workspace/UFO_Project/src/main/java/com/urfavoriteott/ufo/member/model/service/MemberServiceImpl.java
+++ UFO-Workspace/UFO_Project/src/main/java/com/urfavoriteott/ufo/member/model/service/MemberServiceImpl.java
@@ -1,17 +1,78 @@
-package com.urfavoriteott.ufo.member.model.service;
+package com.urfavoriteott.urfavoriteott.member.model.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.urfavoriteott.ufo.member.model.dao.MemberDao;
+import com.urfavoriteott.urfavoriteott.common.model.vo.PageInfo;
+import com.urfavoriteott.urfavoriteott.member.model.dao.MemberDao;
+import com.urfavoriteott.urfavoriteott.member.model.vo.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
-    @Autowired
-    private SqlSessionTemplate sqlSession;
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private MemberDao memberDao;
+	
+	@Override
+	public Member loginMember(Member m) {
+		return memberDao.loginMember(sqlSession, m);
+	}
+	
+	@Override
+	public int nicknameCheck(String checkNickname) {
+		
+		return memberDao.nicknameCheck(sqlSession, checkNickname);
+	}
 
-    @Autowired
-    private MemberDao memberDao;
+	@Override
+	public int updateMember(Member m) {
+		
+		return memberDao.updateMember(sqlSession, m);
+	}
+	
+	@Override
+	public int selectListCount() {
+		
+		return memberDao.selectListCount(sqlSession);
+	}
+
+	@Override
+	public ArrayList<Member> selectList(PageInfo pi) {
+		
+		return memberDao.selectList(sqlSession, pi);
+	}
+
+	@Override
+	public int selectSearchCount(HashMap<String, String> map) {
+		
+		return memberDao.selectSearchCount(sqlSession, map);
+	}
+
+	@Override
+	public ArrayList<Member> selectSearchList(HashMap<String, String> map, PageInfo pi) {
+		
+		return memberDao.selectSearchList(sqlSession, map, pi);
+	}
+
+	@Override
+	public int updatePwd(int userNo) {
+		
+		return memberDao.updatePwd(sqlSession, userNo);
+	}
+
+	@Override
+	public int deleteMember(int userNo) {
+		
+		return memberDao.deleteMember(sqlSession, userNo);
+	}
+
+
+	
 }
